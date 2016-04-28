2b6b87defc8fe4faa2b3e01204f38d14274b0acc
tunlin.tan <tunlin.tan@stargt.com.cn>
2016-04-28 18:48:37 +0800

    clean some code

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,8 @@
   <div class="app">
     <main-header></main-header>
     <side-bar></side-bar>
-
-    <!--<blank-page></blank-page>-->
     <!-- use router-view element as route outlet -->
     <router-view></router-view>
-
     <main-footer></main-footer>
   </div>
 </template>
@@ -16,14 +13,13 @@
 
   import MainHeader from './components/layout/Header'
   import SideBar from './components/layout/SideBar'
-  import BlankPage from './components/Common/BlankPage'
   import MainFooter from './components/layout/Footer'
 
   export default {
     name: 'App',
     store: store,
     components:{
-      MainHeader,SideBar,BlankPage,MainFooter
+      MainHeader,SideBar,MainFooter
     }
   }
 </script>
